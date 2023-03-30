cb1c6e066e5d6938e9df43c907be774a6d283f92
Popiiii <ciriane.vergnaud@etu.iut-tlse3.fr>
2023-03-30 11:35:15 +0200

    Asset menu creation

---
+++ Assets/Code/Menu/CreationPartie.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreationPartie : MonoBehaviour
+{
+    public GameObject DdMois;
+   //private Dropdown[] nombreMois;
+
+    public GameObject DdJoueur;
+    //private Dropdown[] nombreJoueur;
+
+    void Start(){
+    //nombreMois = DdMois.GetComponentsInChildren<Dropdown>();
+    //nombreMois.value
+
+   // nombreJoueur = DdJoueur.GetComponentsInChildren<Dropdown>();
+    //nombreJoueur.value
+
+
+    }
+
+
+}
