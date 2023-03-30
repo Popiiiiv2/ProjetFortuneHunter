07af8b47623f2d97337d8faa27989d69aa77354b
Popiiii <ciriane.vergnaud@etu.iut-tlse3.fr>
2023-03-30 16:02:15 +0200

    Choix joueur et tours

--- Assets/Code/Menu/CreationPartie.cs
+++ Assets/Code/Menu/CreationPartie.cs
@@ -1,19 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CreationPartie : MonoBehaviour
 {
-    public GameObject DdMois;
-   //private Dropdown[] nombreMois;
+    public Dropdown ddMois;
+    private string textMois;
 
-    public GameObject DdJoueur;
-    //private Dropdown[] nombreJoueur;
+    public Dropdown ddJoueur;
+    private string textJoueur;
 
-    void Start(){
-    //nombreMois = DdMois.GetComponentsInChildren<Dropdown>();
-    //nombreMois.value
+ 
+    public GameObject input3;
+    public GameObject input4;
 
+    void Update(){
+    //Recuper la valeur de nombre de mois choisi par les joueurs
+    textMois=ddMois.captionText.text;
+    print(textMois);
+
+    //Recuper la valeur de nombre de joueurs choisi par les joueurs
+    textJoueur=ddJoueur.captionText.text;
+    print(textJoueur);
+
+    switch (textJoueur)
+    {
+        case "2" :
+        if (input3.activeInHierarchy == true || input4.activeInHierarchy == true)
+        {
+        input3.SetActive(false);
+        input4.SetActive(false);
+        }
+        
+        break;
+        case "3" :
+        input3.SetActive(true);
+        if ( input4.activeInHierarchy == true )
+        {
+        input4.SetActive(false);
+        }
+        break;
+        case "4" :
+        input3.SetActive(true);
+        input4.SetActive(true);
+        break;
+    }
+
+    
     }
 }
