901a2770ff068346f4163f8b0f81885e35a1d813
Gapkhan <kevin.fireshaw.rochis@gmail.com>
2023-05-10 08:55:12 +0200

    ajout de la classe score

---
+++ Assets/Code/Score/Score.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score
+{
+    public int montant;
+
+    public int getMontant() {
+        return montant;
+    }
+
+    public void setMontant(int valeur) {
+        montant = valeur;
+    }
+
+}
