399417b6819f892f94df6efd06cf0595f6ac4a97
Crow <fantube82@gmail.com>
2023-05-17 10:58:52 +0200

    cagnotte affichage

--- Assets/Code/Jeu/buttonPig.cs
+++ Assets/Code/Jeu/buttonPig.cs
@@ -8,16 +8,33 @@ public class buttonPig : MonoBehaviour
     private GameObject g;
     public Button monBouton;
     private bool active;
+    private Vector3 initialPosition;
+
     // Start is called before the first frame update
     void Start()
     {
         active = true;
         g = GameObject.Find("BackgroundCagnotte");
+        initialPosition = g.transform.position;
         monBouton.onClick.AddListener(afficherCacher);
     }
 
-    void afficherCacher(){
+   void afficherCacher()
+    {
         active = !active;
-        g.SetActive(active);
+
+        // Changer la position sur l'axe X
+        Vector3 newPosition = initialPosition;
+        if (active)
+        {
+            newPosition.x = 3540;
+        }
+        else
+        {
+            newPosition.x = 1300;
+        }
+
+        // Appliquer la nouvelle position à l'objet
+        g.transform.position = newPosition;
     }
 }
