c999b53c5ba31d89a0c6f18ae95e5f1d1cf9f09c
Vignolles <oscarchat31@gmail.com>
2023-05-22 11:07:17 +0200

    test working

--- Assets/Code/Joueur/HudJoueur.cs
+++ Assets/Code/Joueur/HudJoueur.cs
@@ -15,12 +15,16 @@ public class HudJoueur : MonoBehaviour
 
     public void initialiserScoreJoueur(string score, string textAcqui)
     {
-        this.scoreJoueur = new ScoreJoueur();
+        initialisationScore();
         text = GameObject.Find(score).GetComponent<Text>();
         text.text = scoreJoueur.ToString();
         textAcquisition = GameObject.Find(textAcqui).GetComponent<Text>();
         textAcquisition.text = scoreJoueur.inventaireToString();
         action = new Action();
+    }
+
+    public void initialisationScore(){
+        this.scoreJoueur = new ScoreJoueur();
     }
 
     public void setScore(CarteData carteData)
@@ -46,8 +50,10 @@ public class HudJoueur : MonoBehaviour
 
     public void miseAJourDesScores()
     {
-        text.text = scoreJoueur.ToString();
-        textAcquisition.text = scoreJoueur.inventaireToString();
+        if(text!= null && textAcquisition != null){
+            text.text = scoreJoueur.ToString();
+            textAcquisition.text = scoreJoueur.inventaireToString();
+        }
     }
 
     public void setJeu(Jeu jeu)
